d42fe73b367ff2c2d980e845f695482be4450a02
Akhand-20 <akhandpratapdwivedi9@gmail.com>
2026-07-03 12:10:27 +0530

    added backend link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://ledger-backend-3h87.onrender.com/api",
     withCredentials: true, // sends cookies ->your JWT token ->with every request
 })
 
